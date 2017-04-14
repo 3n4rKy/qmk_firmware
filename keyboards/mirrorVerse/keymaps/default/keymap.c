83ddc0b24e868725f25349a3c15f4fdb6c759808
Benjamin Biedermann <benjamin.biedermann@capgemini.com>
2017-04-14 22:38:11 +0200

    changes layer keys and added media keys

--- keyboards/mirrorVerse/keymaps/default/keymap.c
+++ keyboards/mirrorVerse/keymaps/default/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,KC_LGUI,KC_LALT,        KC_SPC,      KC_NO,KC_RALT,F(0),KC_APP,KC_RCTL), */
         [_BL] = KEYMAP(
         KC_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
-    KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(1),   F(1),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
+    KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(0),   F(0),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_SLSH,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
-    KC_LCTL,KC_LGUI,   F(1),   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
+    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
+    KC_LCTL,KC_LGUI,   KC_BSLS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
 
 
@@ -68,12 +68,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,               KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS),
 */
 	[_AL] = KEYMAP(
-    KC_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
-    KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(1),   F(1),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
+    KC_ESC, KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY,KC_MPRV,KC_MNXT,KC_MSTP,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
+    KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(0),   F(0),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_UP,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_LEFT,   KC_DOWN,   KC_RIGHT,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_SLSH,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
-    KC_LCTL,KC_LGUI,   F(1),   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
+    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
+    KC_LCTL,KC_LGUI,   KC_BSLS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
    /*
    * Primary function layer, mostly the same as the traditional Pok3r layout.
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };*/
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0]  = ACTION_LAYER_MOMENTARY(2),  // Momentary Fn overlay
+    [0]  = ACTION_LAYER_MOMENTARY(1),  // Momentary Fn overlay
     [1]  = ACTION_LAYER_TOGGLE(1),     // Toggle Arrow Layer overlay
     [2]  = ACTION_LAYER_TAP_KEY(2, KC_CAPS), // Tap to toggle caps lock and hold to activate function layer
     [3]  = ACTION_LAYER_TOGGLE(3),     // Toggle Underglow Layer overlay
