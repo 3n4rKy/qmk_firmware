c4fcdccf37d49d423ca1940d7f56cd859aa544a0
Benjamin Biedermann <benjamin.biedermann@capgemini.com>
2017-04-15 16:50:57 +0200

    adds window switching (TAB) to function layer

--- keyboards/mirrorVerse/keymaps/default/keymap.c
+++ keyboards/mirrorVerse/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,KC_BSLS, \
     KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
-    KC_LCTL,KC_LGUI,   KC_BSLS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
+    KC_LCTL,KC_LGUI,   KC_NUBS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
 
 
@@ -70,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_AL] = KEYMAP(
     KC_ESC, KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY,KC_MPRV,KC_MNXT,KC_MSTP,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
     KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(0),   F(0),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
-    KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_UP,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
-    KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_LEFT,   KC_DOWN,   KC_RIGHT,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
+    LALT(KC_TAB),LALT(S(KC_TAB)),   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_UP,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
+    LCTL(KC_TAB),LCTL(S(KC_TAB)),  KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_LEFT,   KC_DOWN,   KC_RIGHT,KC_SCLN,KC_QUOT,KC_BSLS, \
+    KC_LSFT,   KC_Z,   KC_X,   KC_C,   S(KC_INS),   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_BSLS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
    /*
