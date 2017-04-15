2100112188db0dc3a05d47b4b4ef68927f7b3329
Benjamin Biedermann <benjamin.biedermann@capgemini.com>
2017-04-15 21:16:35 +0200

    adds first macro

--- keyboards/mirrorVerse/keymaps/default/keymap.c
+++ keyboards/mirrorVerse/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,KC_BSLS, \
     KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
-    KC_LCTL,KC_LGUI,   KC_NUBS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(1),   F(1),   F(1), KC_APP,KC_RCTL, \
+    KC_LCTL,KC_LGUI,   KC_NUBS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(2),   F(3),   F(4), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
 
 
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(0),   F(0),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_4,   KC_5,   KC_6,   KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_1,   KC_2,   KC_3,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
+    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_0,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_NUBS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(2),   F(3),   F(4), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
 
@@ -113,9 +113,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = KEYMAP(
     KC_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
     KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(0),   F(0),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
-    KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_MS_BTN1,   KC_MS_UP,KC_MS_BTN2,KC_LBRC,KC_RBRC, \
+    KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,KC_MS_BTN1,KC_MS_UP,KC_MS_BTN2,KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_MS_LEFT,   KC_MS_DOWN,   KC_MS_RIGHT,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_MS_WH_UP,  KC_MS_BTN3,  KC_MS_WH_DOWN,KC_SLSH,KC_RSFT,         \
+    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_MS_WH_UP,  KC_MS_BTN3,  KC_MS_WH_DOWN,789uKC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_NUBS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(2),   F(3),   F(4), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
    /*
@@ -145,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
     KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(0),   F(0),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
-    KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,KC_BSLS, \
+    KC_CAPS,   KC_A,   KC_S,   M(0),   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,KC_BSLS, \
     KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_NUBS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(2),   F(3),   F(4), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
@@ -186,9 +186,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       switch(id) {
         case 0:
           if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
+            return MACRO(T(d), T(b), T(2), T( ), T("), END  ); 
           }
         break;
       }
