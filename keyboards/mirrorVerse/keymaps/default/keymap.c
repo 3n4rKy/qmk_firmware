b4188a2b41d06be504e30e53e99a01dc57f39d4f
Benjamin Biedermann <benjamin.biedermann@capgemini.com>
2017-05-02 16:01:20 +0200

    adds print, scrolllock and pause

--- keyboards/mirrorVerse/keymaps/default/keymap.c
+++ keyboards/mirrorVerse/keymaps/default/keymap.c
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         
         //Standard Layer
         [0] = KEYMAP(
-        KC_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
+    KC_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
     KC_GRAVE,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   F(0),   F(0),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   F(1),   F(1),   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G, KC_ESC,KC_BSPC,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_PSLS,KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
+    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,S(KC_7),KC_DELT,  KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_NUBS,   F(1),KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(2),   F(3),   F(4), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
 
@@ -71,13 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 	//Function Layer
     [1] = KEYMAP(
-    KC_ESC, KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY,KC_MPRV,KC_MNXT,KC_MSTP,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,                 \
-    KC_GRAVE,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
-    KC_TAB,S(KC_TAB),KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,  KC_UP,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
-    KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_ESC,KC_BSPC,KC_TRNS,KC_LEFT,KC_DOWN,KC_RIGHT,KC_TRNS,KC_TRNS,KC_TRNS, \
-    KC_LSFT,KC_TRNS,LCTL(KC_X),LCTL(KC_C),S(KC_INS),KC_PSLS,KC_DELT,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,         \
-    KC_LCTL,KC_LGUI,KC_TRNS,KC_TRNS,KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(2),   F(3),   F(4), KC_TRNS,KC_TRNS, \
-                        KC_ENT, KC_END,KC_PGDN, KC_ENT),
+    KC_ESC,  KC_MUTE,   KC_VOLD,   KC_VOLU,  KC_MPLY,KC_MPRV,KC_MNXT,KC_MSTP,  KC_F8,  KC_F9, KC_PSCR,KC_SLCK,KC_PAUS,                 \
+    KC_GRAVE,KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
+    KC_TAB,S(KC_TAB),   KC_TRNS,   KC_TRNS,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,  KC_UP, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
+    KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,KC_TRNS, KC_ESC,KC_BSPC,KC_TRNS,KC_LEFT,KC_DOWN,KC_RIGHT,KC_TRNS,KC_TRNS,KC_TRNS, \
+    KC_LSFT, KC_TRNS,LCTL(KC_X),LCTL(KC_C),S(KC_INS),KC_TRNS,S(KC_7),KC_DELT,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,     \
+    KC_LCTL, KC_LGUI,   KC_TRNS,   KC_TRNS,  KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   F(2),    F(3),   F(4),KC_TRNS,KC_TRNS, \
+                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
    /*
    * Primary function layer, mostly the same as the traditional Pok3r layout.
    * ,-------------------------------------------------------------.
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS,KC_TRNS,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,KC_EQL, \
     KC_TAB,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,   KC_4,   KC_5,   KC_6,   KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS, KC_ESC,KC_BSPC, KC_TRNS,   KC_1,   KC_2,   KC_3,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PSLS,KC_DELT,KC_TRNS,   KC_0,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
+    KC_LSFT,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,S(KC_7),KC_DELT,KC_TRNS,   KC_0,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_NUBS,   KC_TRNS,KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   KC_TRNS,   F(3),   F(4), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
 
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
     KC_TAB,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MS_BTN1,KC_MS_UP,KC_MS_BTN2,KC_P,KC_LBRC,KC_RBRC, \
     KC_CAPS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_ESC,KC_BSPC,KC_TRNS,   KC_MS_LEFT,   KC_MS_DOWN,   KC_MS_RIGHT,KC_SCLN,KC_QUOT,KC_BSLS, \
-    KC_LSFT, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PSLS,KC_DELT,KC_TRNS,   KC_MS_WH_UP,  KC_MS_BTN3,  KC_MS_WH_DOWN,KC_SLSH,KC_RSFT,         \
+    KC_LSFT, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,S(KC_7),KC_DELT,KC_TRNS,   KC_MS_WH_UP,  KC_MS_BTN3,  KC_MS_WH_DOWN,KC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_NUBS,KC_TRNS,KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   KC_TRNS,   KC_TRNS,   F(4), KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
    /*
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
     KC_TAB,  KC_TRNS,   M(1),KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
     KC_CAPS,KC_TRNS,   M(2),   M(0),   M(3),KC_TRNS, KC_ESC,KC_BSPC,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
-    KC_LSFT,   KC_TRNS,   KC_TRNS,   M(4),   KC_TRNS,   KC_B,KC_PSLS,KC_DELT,  KC_TRNS,   KC_TRNS,KC_TRNS, KC_TRNS,KC_SLSH,KC_RSFT,         \
+    KC_LSFT,   KC_TRNS,   KC_TRNS,   M(4),   KC_TRNS,   KC_B,S(KC_7),KC_DELT,  KC_TRNS,   KC_TRNS,KC_TRNS, KC_TRNS,KC_SLSH,KC_RSFT,         \
     KC_LCTL,KC_LGUI,   KC_NUBS,   KC_TRNS,KC_LALT, KC_SPC,KC_HOME,KC_PGUP, KC_SPC,KC_RALT,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_APP,KC_RCTL, \
                         KC_ENT, KC_END,KC_PGDN, KC_ENT),
 };
@@ -186,27 +186,27 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       switch(id) {
         case 0:
           if (record->event.pressed) {
-            return MACRO(T(D), T(B), T(2), T(SPC), D(LSFT), T(2), U(LSFT), D(LSFT), T(2), U(LSFT), T(LEFT), END  ); 
+            return MACRO(T(D), T(B), T(2), T(SPC), D(LSFT), T(2), U(LSFT), D(LSFT), T(2), U(LSFT), T(LEFT), END  ); //db2 ""
           }
         break;
         case 1:
           if (record->event.pressed) {
-            return MACRO(D(LSFT), T(W), T(H), T(E), T(R), T(E), U(LSFT), T(SPC), END  ); 
+            return MACRO(D(LSFT), T(W), T(H), T(E), T(R), T(E), U(LSFT), T(SPC), END  ); //WHERE
           }
         break;
         case 2:
           if (record->event.pressed) {
-            return MACRO(D(LSFT), T(S), T(E), T(L), T(E), T(C), T(T), U(LSFT), T(SPC), END  ); 
+            return MACRO(D(LSFT), T(S), T(E), T(L), T(E), T(C), T(T), U(LSFT), T(SPC), END  ); //SELECT
           }
         break;
         case 3:
           if (record->event.pressed) {
-            return MACRO(D(LSFT), T(F), T(R), T(O), T(M), U(LSFT), T(SPC), END  ); 
+            return MACRO(D(LSFT), T(F), T(R), T(O), T(M), U(LSFT), T(SPC), END  );// FROM
           }
         break;
 		case 4:
           if (record->event.pressed) {
-            return MACRO(T(D), T(B), T(2), T(SPC), T(C), T(O), T(N), T(N) , T(E) , T(C), T(T), T(SPC), T(T), T(O), T(SPC),  END  ); 
+            return MACRO(T(D), T(B), T(2), T(SPC), T(C), T(O), T(N), T(N) , T(E) , T(C), T(T), T(SPC), T(T), T(O), T(SPC),  END  ); //db2 connect to
           }
         break;
              }
